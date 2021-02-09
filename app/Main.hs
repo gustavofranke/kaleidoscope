dd921fb33574db5eedbcb14c6e68e1c080658564
Gustavo Franke <franke.gustavo@gmail.com>
2021-02-09 17:25:19 +0000

    add changes to main module

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,22 @@
 module Main where
 
-import Lib
+import Parser
+
+import Control.Monad.Trans
+import System.Console.Haskeline
+
+process:: String -> IO ()
+process line = do
+    let res = parseTopLevel line
+    case res of
+        Left err -> print err
+        Right ex -> mapM_ print ex
 
 main :: IO ()
-main = someFunc
+main = runInputT defaultSettings loop
+    where
+        loop = do
+            minput <- getInputLine "ready> "
+            case minput of
+                Nothing -> outputStr  "Goodbye."
+                Just input -> (liftIO $ process input) >> loop
